564591bfa2a6856055f52d4c482e9bec86ec5ca1
Jason Jalbuena <forlogos@users.noreply.github.com>
2015-01-18 21:49:58 -0500

    Create embed-Adobe-FormsCentral.php

---
+++ embed-Adobe-FormsCentral.php
@@ -0,0 +1,11 @@
+<?php /*
+Plugin Name: Embed Adobe FormsCentral
+Description: Easily Embed Adobe FormsCentral forms in your posts, pages, and custom post types
+Author: forlogos
+Author URI: https://jasonjalbuena.com/
+Requires at least: 3.6
+Version: 1.0
+*/
+
+
+?>
