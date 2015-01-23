914ec904f3828f993a700122083d96bf9f688849
forlogos <jason.jalbuena@gmail.com>
2015-01-23 01:37:41 -0500

    make a working shortcode

--- embed-Adobe-FormsCentral.php
+++ embed-Adobe-FormsCentral.php
@@ -24,6 +24,12 @@ class flgs_embed_adobe_formscentral {
 	//Initializes the plugin by setting hooks, filters, and functions.
 	private function __construct() {
 		$this->plugin_url = plugin_dir_url( __FILE__ );
+		//add_action('admin_init', array($this,'register_settings'));		
+		//add_action('admin_enqueue_scripts', array($this,'tinymce_buttons'));
+		//add_action('admin_footer', array( $this, 'print_scripts_styles'));
+
+		add_shortcode('adobeform', array($this,'shortcode_output'));
+
 		$this->run_plugin();
 	}
 
@@ -31,8 +37,52 @@ class flgs_embed_adobe_formscentral {
     	return $this->plugin_url;
     }
 
+    public function tinymce_buttons() {
+		add_filter('mce_external_plugins','add_buttons');
+    	add_filter('mce_buttons','register_buttons');
+	}
+
+	
+	public function add_buttons($plugin_array) {
+		
+    	return $plugin_array;
+	}
+
+	public function register_buttons($buttons) {
+		
+    	return $buttons;
+	}
+
+	//let the shortcode show our adobe form
+	function shortcode_output($atts){
+		//get attributes
+		extract( shortcode_atts(
+			array(
+				'js' => 'https://formscentral.acrobat.com/Clients/Current/FormsCentral/htmlClient/scripts/adobe.form.embed.min.js',
+				'formid' => ''
+			), $atts )
+		);
+
+		//ensure there is a formId, else return
+		if(empty($formid) || $formid=='') {
+			return;
+		}
+
+		//output js
+		$return='<script type="text/javascript" src="'.$js.'"></script>';
+
+		//output id
+		$return.='<script type="text/javascript">;ADOBEFORMS.EmbedForm({formId:"'.$formid.'"});</script>';
+
+		//return all output
+		return $return;
+	}
+
+
     //plugin's functionality functions
     private function run_plugin() {
+
+
 
 	}
 }
